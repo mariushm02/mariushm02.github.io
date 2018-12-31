7804a1fd776f96a8b6561b764d8d00d2b7b34ea5
Marius <mariusstegaru6@gmail.com>
2018-12-31 02:11:37 +0200

    Add files via upload

---
+++ newyear/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    //DECLARATIONS
+    var noYear = new Audio();
+    noYear.src = "audio.mp3";
+    let currentDate = new Date();
+    let currentDay = currentDate.getDate();
+    let countDownDate = new Date("Dec 31, 2018 02:06:50").getTime();
+    let daysDiv = document.getElementById('days');
+    let hoursDiv = document.getElementById('hours');
+    let minutesDiv = document.getElementById('minutes');
+    let secondsDiv = document.getElementById('seconds');
+    let isMobile = false; //initiate as false
+    noYear.pause();
+    noYear.play();
+    $('#canvas').css("display", "none");
+    $('#canvas-countdown').css('display', 'block');
+    let x = setInterval(function () {
+
+        let now = new Date().getTime();
+
+        let distance = countDownDate - now;
+
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        daysDiv.innerHTML = days + "<div class='abs'><span>days</span></div> <div class='wrap-flame daysFlame'><div class='flame-wrapper'><div class='flame red'></div><div class='flame orange'></div><div class='flame gold'></div><div class='flame white'></div></div></div>";
+        hoursDiv.innerHTML = hours + "<div class='abs'><span>hours</span></div></div> <div class='wrap-flame hoursFlame'><div class='flame-wrapper'><div class='flame red'></div><div class='flame orange'></div><div class='flame gold'></div><div class='flame white'></div></div></div>";
+        minutesDiv.innerHTML = minutes + "<div class='abs'><span>minutes</span></div> <div class='wrap-flame minutesFlame'><div class='flame-wrapper'><div class='flame red'></div><div class='flame orange'></div><div class='flame gold'></div><div class='flame white'></div></div></div>";
+        secondsDiv.innerHTML = seconds + "<div class='abs'><span>seconds</span></div> <div class='wrap-flame secondsFlame'><div class='flame-wrapper'><div class='flame red'></div><div class='flame orange'></div><div class='flame gold'></div><div class='flame white'></div></div></div>";
+
+
+        if (distance <= 0) {
+            clearInterval(x);
+            daysDiv.innerHTML = 0 + "<div class='abs'><span>days</span></div> <div class='wrap-flame daysFlame'><div class='flame-wrapper'><div class='flame red'></div><div class='flame orange'></div><div class='flame gold'></div><div class='flame white'></div></div></div>";
+            hoursDiv.innerHTML = 0 + "<div class='abs'><span>hours</span></div></div> <div class='wrap-flame hoursFlame'><div class='flame-wrapper'><div class='flame red'></div><div class='flame orange'></div><div class='flame gold'></div><div class='flame white'></div></div></div>";
+            minutesDiv.innerHTML = 0 + "<div class='abs'><span>minutes</span></div> <div class='wrap-flame minutesFlame'><div class='flame-wrapper'><div class='flame red'></div><div class='flame orange'></div><div class='flame gold'></div><div class='flame white'></div></div></div>";
+            secondsDiv.innerHTML = 0 + "<div class='abs'><span>seconds</span></div> <div class='wrap-flame secondsFlame'><div class='flame-wrapper'><div class='flame red'></div><div class='flame orange'></div><div class='flame gold'></div><div class='flame white'></div></div></div>";
+            daysDiv.classList.add('countdown-finalDays');
+            hoursDiv.classList.add('countdown-finalHours');
+            minutesDiv.classList.add('countdown-finalMinutes');
+            secondsDiv.classList.add('countdown-finalSeconds');
+            setTimeout(function () {
+                $('#canvas-countdown').css('display', 'none');
+                $('#canvas').css('display', 'flex');
+            }, 1000)
+
+// device detection
+
+        }
+    }, 1000);
+
+    for (i = 0; i < 75; i++){
+        $(".stars-wrapper").append("<div class='stars s" + i + "'></div>");
+}
+
+});
